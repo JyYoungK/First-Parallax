dc422e5746bde210646e1aba51fe1a0baf80dd9d
Johnny Kang <junyoung@my.yorku.ca>
2022-12-22 01:12:43 -0500

    Paralax

--- src/App.js
+++ src/App.js
@@ -27,29 +27,63 @@ function App() {
     const s = window.scrollY;
     const w = window.outerWidth;
     const h = document.getElementsByClassName("paralax")[0].clientWidth;
-    const h_b = document.getElementsByClassName("container")[0].clientWidth;
-    const p = (s / h) * 1;
+    const h_b =
+      document.getElementsByClassName("forest_container")[0].clientWidth;
+    const p = (s / h) * 1000;
     const p_b = (s / h_b) * 100;
-    const opas = 1 - (1 / 100) * p_b;
-    const z_1 = 1 + (w / 10000) * p_b;
-    const z_2 = 1 + (w / 5000000) * p;
-    document.getElementsByClassName(
-      "p-item1"
-    )[0].style = `transform: scale(${z_2})`;
-    const hr = (w / 2000) * p_b;
-    const z_3 = 1 + w * 0.000005 * p_b;
-    document.getElementsByClassName(
-      "p-item2"
-    )[0].style = `transform: translate3d(${hr}px,0,0) scale(${z_3})`;
-    const hr_2 = (w / 1500) * p_b;
+    const opas = 1 - (1 / 100) * p_b; //scale fastest
+    const z_1 = 1 + (w / 1000) * p_b;
+    const z_2 = 1 + (w / 10000) * p_b;
+    const z_3 = 1 + (w / 90000) * p_b;
     const z_4 = 1 + w * 0.00001 * p_b;
+    const z_forest = 1 + w * 0.000008 * p_b;
+    const z_6 = 1 + w * 0.000001 * p_b;
+    const z_7 = 1 + w * 0.0000005 * p_b;
+    const z_8 = 1 + w * 0.00000001 * p_b;
+    const z_9 = 1 + w * 0.000005 * p_b;
+    const z_10 = 1 + (w / 5000000) * p; //scale slowest
+    const hr = (w / 1500) * p_b; //scale fastest
+    const hr_2 = (w / 15000) * p_b;
+
     document.getElementsByClassName(
-      "p-item3"
-    )[0].style = `transform: translate3d(${hr_2}px,0,0) scale(${z_4})`;
-    document.getElementsByClassName(
-      "p-item4"
-    )[0].style = `transform: scale(${z_1});opacity: ${opas}`;
+      "moon"
+    )[0].style = `transform: scale(${z_10})`;
+
+    // document.getElementsByClassName(
+    //   "fog"
+    // )[0].style = `transform: scale(${z_1});opacity: ${opas}`;
+
+    //forest starts scaling, others don't
+    console.log(z_forest);
+
+    if (z_forest <= 4) {
+      document.getElementsByClassName(
+        "forest"
+      )[0].style = `transform: translate3d(${hr}px,0,0) scale(${z_forest})`;
+      document.getElementsByClassName(
+        "temple"
+      )[0].style = `transform: translate3d(${hr_2}px,0,0) scale(${z_7})`;
+    }
+
+    if (z_forest <= 15 && z_forest > 4) {
+      //start scaling after forest finishes scaling
+      document.getElementsByClassName(
+        "temple"
+      )[0].style = `transform: translate3d(${hr_2}px,0,0) scale(${z_6})`;
+    }
+
+    if (z_forest > 15) {
+      //start scaling fast after forest content finishes
+      document.getElementsByClassName(
+        "temple"
+      )[0].style = `transform: translate3d(${hr_2}px,0,0) scale(${z_4})`;
+    }
+
+    // document.getElementsByClassName(
+    //   "deer"
+    // )[0].style = `transform: translate3d(${hr_3}px,0,0) scale(${z_forest})`;
   };
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
@@ -64,142 +98,114 @@ function App() {
 
   return (
     <div>
-      {/* <div className="app bg-deep-blue"> */}
-      {/* <div className="paralax">
-        <div className="fixed w-full h-full top-0 left-0 p-item1"></div>
-        <div className="fixed w-full h-full top-0 left-0 p-item2"></div>
-        <div className="fixed w-full h-full top-0 left-0 p-item3"></div>
-        <div className="fixed w-full h-full top-0 left-0 p-item4"></div>
+      <div className="paralax">
+        {/* <div className="fixed w-full h-full top-0 left-0 fog"></div> */}
+        <div className="fixed w-full h-full top-0 left-0 moon"></div>
+        {/* ----------- First Content Title/Forest Page -----------  */}
+        <div className="fixed w-full h-full top-0 left-0 forest"></div>
+        <div className="forest_container">
+          <div className="forest-header">
+            <h1> Junyoung Kang</h1>
+            <h2> Frontend Developer/Web Designer</h2>
+          </div>
+          <div className="content"></div>
+        </div>
+
+        {/* ----------- Second Content /Temple Page ----------- */}
+        <div className="fixed w-full h-full top-0 left-0 temple"></div>
+        <div className="temple_container">
+          <div className="w-5/6 mx-auto md:h-full ">
+            <motion.div
+              margin="0 0 -200px 0"
+              amount="all"
+              onViewportEnter={() => setSelectedPage("skills")}
+            >
+              <MySkills />
+            </motion.div>
+          </div>
+          <LineGradient />
+          <div className="w-5/6 mx-auto">
+            <motion.div
+              margin="0 0 -200px 0"
+              amount="all"
+              onViewportEnter={() => setSelectedPage("projects")}
+            >
+              <Projects />
+            </motion.div>
+          </div>
+          <LineGradient />
+          <div className="w-5/6 mx-auto md:h-full">
+            <motion.div
+              margin="0 0 -200px 0"
+              amount="all"
+              onViewportEnter={() => setSelectedPage("testimonials")}
+            >
+              <Testimonials />
+            </motion.div>
+          </div>
+        </div>
+
+        {/* ----------- Third Content /Deer Page ----------- */}
+        <div className="fixed w-full h-full top-0 left-0 deer"></div>
+        <div className="deer_container">
+          <div className="w-5/6 mx-auto md:h-full ">
+            <motion.div
+              margin="0 0 -200px 0"
+              amount="all"
+              onViewportEnter={() => setSelectedPage("skills")}
+            >
+              <MySkills />
+            </motion.div>
+          </div>
+          <LineGradient />
+          <div className="w-5/6 mx-auto">
+            <motion.div
+              margin="0 0 -200px 0"
+              amount="all"
+              onViewportEnter={() => setSelectedPage("projects")}
+            >
+              <Projects />
+            </motion.div>
+          </div>
+          <LineGradient />
+          <div className="w-5/6 mx-auto md:h-full">
+            <motion.div
+              margin="0 0 -200px 0"
+              amount="all"
+              onViewportEnter={() => setSelectedPage("testimonials")}
+            >
+              <Testimonials />
+            </motion.div>
+          </div>
+          {/* <div className="w-5/6 mx-auto md:h-full">
+            <motion.div
+              margin="0 0 -200px 0"
+              amount="all"
+              onViewportEnter={() => setSelectedPage("contact")}
+            >
+              <Contact />
+            </motion.div>
+          </div>
+          <Footer /> */}
+        </div>
+        {/* End */}
       </div>
-      <div className="container">
-        <div className="content-header">
-          <h1> Parallax Design</h1>
-          <h2> The next Level Parallax Scrolling Web Design</h2>
-        </div>
 
-        <div className="content">
-          <img src="./assets/contact-image.jpeg" alt="contact" />
-
-          <img src="./assets/brush.png" alt="astro" />
-        </div>
+      {/* <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("home")}
+        >
+          <Landing setSelectedPage={setSelectedPage} />
+        </motion.div>
       </div> */}
-      <div className="w-5/6 mx-auto md:h-full">
-        {isDesktop && (
-          <DotGroup
-            selectedPage={selectedPage}
-            setSelectedPage={setSelectedPage}
-          />
-        )}
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("home")}
-        >
-          <Landing setSelectedPage={setSelectedPage} />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full ">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("skills")}
-        >
-          <MySkills />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("projects")}
-        >
-          <Projects />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("testimonials")}
-        >
-          <Testimonials />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("contact")}
-        >
-          <Contact />
-        </motion.div>
-      </div>
-      <Footer />
-      {/* <Navbar
-        isTopOfPage={isTopOfPage}
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
-      <div className="w-5/6 mx-auto md:h-full">
-        {isDesktop && (
-          <DotGroup
-            selectedPage={selectedPage}
-            setSelectedPage={setSelectedPage}
-          />
-        )}
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("home")}
-        >
-          <Landing setSelectedPage={setSelectedPage} />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full ">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("skills")}
-        >
-          <MySkills />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("projects")}
-        >
-          <Projects />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("testimonials")}
-        >
-          <Testimonials />
-        </motion.div>
-      </div>
-      <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("contact")}
-        >
-          <Contact />
-        </motion.div>
-      </div>
-      <Footer /> */}
     </div>
   );
 }
